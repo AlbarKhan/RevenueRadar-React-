a7c7ced669464f30247746ce6a4230301a3cbcc5
unknown <albarkhan60@gmail.com>
2024-02-16 17:16:58 +0530

    layout  done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 }
@@ -17,7 +18,7 @@ function Header() {
           <img
             className="logo-img"
             src="https://cdn.dribbble.com/users/2407143/screenshots/10717420/media/60aab191ea1165e667a9f2edd6375105.png"
-            // style={{ width: "80px", borderRadius: "20px" }}
+            alt="logo"
           ></img>
           Revenue Radar
         </span>
@@ -35,5 +36,46 @@ function Header() {
         </span>
       </nav>
     </header>
+  );
+}
+
+function Body() {
+  return (
+    <div className="main-wrapper">
+      <div className="main-content">
+        <div className="list">
+          <Table />
+        </div>
+        <div className="inputForm">
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <form className="form">
+      <div>
+        <input className="name" placeholder="Name"></input>
+        <input type="date" className="name" placeholder="Name"></input>
+        <input type="number" className="name" placeholder="Amount "></input>
+      </div>
+    </form>
+  );
+}
+
+function Table() {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th className="date-column">Date</th>
+          <th className="name-column">Name</th>
+          <th className="amount-column">Amount</th>
+        </tr>
+      </thead>
+    </table>
   );
 }
