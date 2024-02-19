c3b3601d2e1bcc950669899258461d4bc4b24c8e
unknown <albarkhan60@gmail.com>
2024-02-19 12:34:32 +0530

    layout responsive

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import { useState } from "react";
 
+//ignore prettier
+const months = [
+  "January",
+  "Feburary",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 const data = [
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
-  { username: "Albar Khan", date: "23-02-24", amount: [786000] },
+  { username: "Albar Khan", date: "23", year: 2024, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2024, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2024, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2024, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2024, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2021, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2021, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2021, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2022, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2023, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2023, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2023, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2023, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2023, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2023, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2021, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2023, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2022, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2022, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2022, amount: 786000 },
+  { username: "Albar Khan", date: "23", year: 2021, amount: 786000 },
 ];
 export default function App() {
   return (
@@ -79,11 +94,42 @@ function Body() {
 }
 
 function Form() {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+
+  function getCurrentDate() {
+    const today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1; // January is 0
+    const yyyy = today.getFullYear();
+
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+
+    return yyyy + "-" + mm + "-" + dd;
+  }
+
   return (
     <form className="form">
       <div>
-        <input className="name" placeholder="Name"></input>
-        <input type="date" className="name" placeholder="Name"></input>
+        <input
+          className="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <input
+          type="date"
+          className="name"
+          placeholder="Name"
+          min={getCurrentDate()}
+          onChange={(e) => setDate(e.target.value)}
+        ></input>
         <input type="number" className="name" placeholder="Amount "></input>
       </div>
     </form>
@@ -91,14 +137,23 @@ function Form() {
 }
 
 function DateBar() {
+  let years = new Set();
+  data.forEach((element) => {
+    years.add(element.year);
+  });
+  // console.log(years);
   return (
     <div className="date-bar">
       <div className="select-options">
         <select>
-          <option>Year</option>
+          {[...years].map((year, index) => (
+            <option key={index}>{year}</option>
+          ))}
         </select>
         <select>
-          <option>month</option>
+          {months.map((month, index) => (
+            <option>{month}</option>
+          ))}
         </select>
       </div>
       <div>
@@ -110,23 +165,25 @@ function DateBar() {
 
 function Table() {
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th className="date-column">Date</th>
-          <th className="name-column">Name</th>
-          <th className="amount-column">Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((data) => (
-          <tr key={data.username}>
-            <td>{data.date}</td>
-            <td>Albar Khan</td>
-            <td>₹{data.amount}</td>
+    <div className="table-parent">
+      <table className="table">
+        <thead>
+          <tr>
+            <th className="date-column">Date</th>
+            <th className="name-column">Name</th>
+            <th className="amount-column">Amount</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="table-body">
+          {data.map((data, index) => (
+            <tr key={index}>
+              <td>{data.date}</td>
+              <td>Albar Khan</td>
+              <td>₹{data.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
